74435f1c9358a18ba7658dd2962220bc4a8a2c38
Arpit Choudhary <arpitchoudhary112006@gmail.com>
2025-08-27 09:32:13 +0530

    exception

---
+++ src/calculator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter any two number/");
+        System.out.println("Enter value of x: ");
+        int x=input.nextInt();
+        System.out.println("Enter value of y: ");
+        int y=input.nextInt();
+        int z=0;
+
+        try{
+            z=x/y;
+        }
+        catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println(z);
+        }
+    }
+}
