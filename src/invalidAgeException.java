6541f6c2587afb27f967c7898bf43340fcb82563
Arpit Choudhary <arpitchoudhary112006@gmail.com>
2025-08-28 09:42:59 +0530

    Customize exception

---
+++ src/invalidAgeException.java
@@ -0,0 +1,8 @@
+//STEP 1
+public class invalidAgeException extends Exception {
+//    STEP 2
+    public invalidAgeException(String message){
+        super(message);
+    }
+
+}
---
+++ src/userDefinedException.java
@@ -0,0 +1,11 @@
+//STEP 2 Continuous
+public class userDefinedException {
+    public void validate(int age) throws invalidAgeException{
+        if(age<18){
+            throw new invalidAgeException("Not eligible to vote");
+        }
+        else{
+            System.out.println("Eligible to vote");
+        }
+    }
+}
