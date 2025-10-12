484a9eb060347ede53a57b2611d9a7b0e2988f0f
Arpit Choudhary <arpitchoudhary112006@gmail.com>
2025-10-12 19:41:39 +0530

    Custom Exception

--- src/unit2/myMainThread1.java
+++ src/unit2/myMainThread1.java
@@ -4,6 +4,9 @@ public class myMainThread1 {
     public static void main(String[] args) {
         Thread1 th1=new Thread1();
         Thread2 th2=new Thread2();
+        th1.setPriority(Thread.MAX_PRIORITY);
+        System.out.println(th1.getPriority());
+        System.out.println(th2.getPriority());
         th1.start();
         th2.start();
     }
